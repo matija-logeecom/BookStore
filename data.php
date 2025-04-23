64a09279077fb8a85eca585769fba6a835ad2316
Matija Stankovic <matija.stankovic@logeecom.com>
2025-04-23 13:09:37 +0200

    add author data

---
+++ data.php
@@ -0,0 +1,8 @@
+<?php
+$authors = [
+    ['id' => 1, 'name' => 'Pera Peric', 'books' => 6],
+    ['id' => 2, 'name' => 'Mika Mikic', 'books' => 2],
+    ['id' => 3, 'name' => 'Zika Zikic', 'books' => 3],
+    ['id' => 4, 'name' => 'Nikola Nikolic', 'books' => 0],
+
+];
